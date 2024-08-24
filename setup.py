0313961828b174f9621bb4de58227581f59627c3
chetan chowdhari <chetanchaudhari90014@gmail.com>
2024-08-24 21:29:21 +0530

    removed logger & exception code

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 
 def get_requirements(file_path:str)-> list[str]:
@@ -13,7 +13,7 @@ def get_requirements(file_path:str)-> list[str]:
 setup(
     name='mlproject',
     version='0.0.1',
-    author='chetan',
+    author='chetan chowdhari',
     author_email='chetan@gmail.com',
     packages=find_packages(),
     install_requires= get_requirements('requirements.txt')
